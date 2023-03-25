709c7efbf0199225714b0f5d0ff01c59c9f577ad
Limko <limkoshio@gmail.com>
2023-03-25 16:15:13 +0800

    added sticky zone

--- Assets/Entity/Mechanism/MovingEternally/ObjectMovingEternally.cs
+++ Assets/Entity/Mechanism/MovingEternally/ObjectMovingEternally.cs
@@ -2,51 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMovingEternally : MonoBehaviour
+namespace LanternTrip
 {
-    // Start is called before the first frame update
-    public Transform startPosition;
-    public Transform endPosition;
-    private Transform targetPosition;
-    public Transform objectToMove;
-    [Range(0, 5)] public float waitTime;
-    public float timer = 0;
-    [Range(0, 5)] public float moveSpeed;
-    void Start()
-    {
-        targetPosition = endPosition;
-        moveSpeed = 0.3f;
-        waitTime = 1.0f;
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public class ObjectMovingEternally : MonoBehaviour
     {
-        float distance = Vector3.Magnitude((objectToMove.position - targetPosition.position));
-        if (distance > 0.1f)//move
+        // Start is called before the first frame update
+        public Transform startPosition;
+        public Transform endPosition;
+        private Transform targetPosition;
+        public Entity objectToMove;
+        [Range(0, 5)] public float waitTime;
+        public float timer = 0;
+        [Range(0, 20)] public float moveSpeed;
+
+        void Start()
         {
-            Move();          
-        }
-        else if(timer<waitTime)//Suspend
-        {
-            Suspend();
+            targetPosition = endPosition;
+            moveSpeed = 10.0f;
+            waitTime = 1.0f;
         }
 
-        else
+        // Update is called once per frame
+        void FixedUpdate()
         {
-            timer = 0;
-            targetPosition = targetPosition==endPosition? startPosition:endPosition;
+            float distance = Vector3.Magnitude((objectToMove.transform.position - targetPosition.position));
+            if (distance > 0.1f) //move
+            {
+                Move();
+            }
+            else if (timer < waitTime) //Suspend
+            {
+                Suspend();
+            }
+
+            else
+            {
+                timer = 0;
+                targetPosition = targetPosition == endPosition ? startPosition : endPosition;
+            }
         }
-    }
 
-    void Move()
-    {
-        objectToMove.localPosition = Vector3.MoveTowards(objectToMove.localPosition, targetPosition.localPosition, moveSpeed * Time.deltaTime);
+        void Move()
+        {
+            var moveTowards = Vector3.MoveTowards(objectToMove.transform.position, targetPosition.position,
+                moveSpeed * Time.deltaTime);
+            objectToMove.Rigidbody.MovePosition(moveTowards);
+        }
 
-    }
-
-    void Suspend()
-    {
-        timer += Time.deltaTime;
+        void Suspend()
+        {
+            timer += Time.deltaTime;
+        }
     }
 }
