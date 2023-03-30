b9e35d6a321095d6189f6ab6d036bf1dc6b59c91
K1riZz <pruprublue@gmail.com>
2023-03-30 22:23:59 +0800

    LevelUpdate0.1

---
+++ Assets/ThirdPartys/E3DMeshPaint/Scripts/E3DMeshPainter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+#if UNITY_EDITOR 
+//[HelpURL("https://www.element3ds.com/forum.php?mod=viewthread&tid=224951&_dsign=e6ec6aa6")]
+[RequireComponent(typeof(MeshCollider))]
+public class E3DMeshPainter : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+  
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+#endif
