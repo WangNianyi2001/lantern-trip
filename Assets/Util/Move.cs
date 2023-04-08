2286bfc6720946036cbc191ba12d2177f4098d98
K1riZz <pruprublue@gmail.com>
2023-04-08 17:39:59 +0800

    Add assets

--- Assets/Util/Move.cs
+++ Assets/Util/Move.cs
@@ -5,16 +5,42 @@ namespace LanternTrip {
 		public PathNode[] path = new PathNode[0];
 		public Transform target;
 		private Path path_test;
+	
+    public enum MoveMode
+	{
+		Alternate = Path.MoveMode.Alternate ,
+		Normal    = Path.MoveMode.Normal ,
+		Reverse   = Path.MoveMode.Reverse,
+		Loop      = Path.MoveMode.Loop,
+	};
+
+	public MoveMode SelectMoveMode;
 		
 		void Start() {
+			SelectMoveMode = MoveMode.Normal;
+
 			path_test = new Path();
 			path_test.Init(path, target);
 
-			path_test.MoveAlongPath(Path.MoveMode.Alternate);
+		if(SelectMoveMode == MoveMode.Normal)
+			{path_test.MoveAlongPath(Path.MoveMode.Normal);
 		}
+
+		else if(SelectMoveMode == MoveMode.Alternate)
+			{path_test.MoveAlongPath(Path.MoveMode.Alternate);
+		}
+
+		else if(SelectMoveMode == MoveMode.Reverse)
+			{path_test.MoveAlongPath(Path.MoveMode.Reverse);
+		}
+
+		else if(SelectMoveMode == MoveMode.Loop)
+			{path_test.MoveAlongPath(Path.MoveMode.Loop);
+		}
+
 		void OnDrawGizmos() {
 			path_test?.Display();
 		}
 	}
 }
-
+}
