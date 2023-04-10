98f2ca250a1df73c4d473854229f0a438dc4945c
Nianyi Wang <Wangnianyi2001@outlook.com>
2023-04-10 09:55:53 +0800

    Add forever parameter for path movers

--- Assets/Mechanism/MoveAlongPath/MoveAlongPath.cs
+++ Assets/Mechanism/MoveAlongPath/MoveAlongPath.cs
@@ -8,12 +8,15 @@ namespace LanternTrip {
 		public bool reversed;
 		public bool alternate;
 		public bool repeat;
+		public bool forever;
 		public bool beginOnEnable;
 		[Expandable] [Instance] public MovingPath path;
 
 		Coroutine movingCoroutine = null;
 		float progress = 0;
 		float direction = 1;
+		float paused = 1;
+		int bounceLeft = 0;
 
 		void UpdateProgress(float progress) {
 			transform.position = path.Position(progress);
@@ -33,11 +36,16 @@ namespace LanternTrip {
 			if(movingCoroutine == null)
 				return false;
 
-			float expectedProgress = Progress + Time.fixedDeltaTime * speed * direction;
+			float delta = Time.fixedDeltaTime * speed;
+			delta *= direction;
+			delta *= paused;
+			float expectedProgress = Progress + delta;
 			Progress = expectedProgress;
 			if(Progress != expectedProgress) {  // µΩÕ∑¡À
-				if(!alternate)
-					return false;
+				if(!forever) {
+					if(--bounceLeft <= 0)
+						return false;
+				}
 				direction *= -1;
 			}
 
@@ -65,8 +73,16 @@ namespace LanternTrip {
 				direction = -1;
 				Progress = path.MaxProgress;
 			}
+			if(!forever)
+				bounceLeft = alternate ? 2 : 1;
 			movingCoroutine = StartCoroutine(MovingCoroutine());
 		}
+
+		[ContextMenu("Pause Moving")]
+		public void PauseMoving() => paused = 0;
+
+		[ContextMenu("Continue Moving")]
+		public void ContinueMoving() => paused = 1;
 
 		[ContextMenu("Stop Moving")]
 		public void StopMoving() {
