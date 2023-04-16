7335da033de9b95e9e92700e255ee8167268469f
K1riZz <pruprublue@gmail.com>
2023-04-16 11:32:50 +0800

    rotation tool update && levelBegin minor update

--- Assets/Mechanism/ControlObjectRotation.cs
+++ Assets/Mechanism/ControlObjectRotation.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class ControlObjectRotation : MonoBehaviour
 {
     private bool isRotating = false;
-    [SerializeField] private float rotationSpeed = 90f;
+    [SerializeField] private float rotationAngle = 90f;
     [SerializeField] private float rotationTime = 1f;
     [SerializeField] private Vector3 rotationAxis = Vector3.up;
+    [SerializeField] private bool RotateAroundOther = false;
+    public GameObject targetObject;
+    
 
     public void RotateObject()
     {
         if (!isRotating)
         {
-            StartCoroutine(RotateCoroutine());
+            switch(RotateAroundOther)
+            {
+                case false:
+                StartCoroutine(RotateCoroutine());
+                    break;
+                    case true:
+                    StartCoroutine(RotateAroundOtherCoroutine());
+                    break;   
+                        }
+            ;
         }
     }
 
@@ -22,7 +34,7 @@ public class ControlObjectRotation : MonoBehaviour
         isRotating = true;
         float elapsedTime = 0f;
         Quaternion startingRotation = transform.rotation;
-        Quaternion targetRotation = transform.rotation * Quaternion.AngleAxis(rotationSpeed, rotationAxis);
+        Quaternion targetRotation = transform.rotation * Quaternion.AngleAxis(rotationAngle, rotationAxis);
 
         while (elapsedTime < rotationTime)
         {
@@ -34,6 +46,25 @@ public class ControlObjectRotation : MonoBehaviour
         transform.rotation = targetRotation;
         isRotating = false;
     }
+
+    private IEnumerator RotateAroundOtherCoroutine()
+    {
+        isRotating = true;
+        Vector3 targetPosition = targetObject.transform.position;
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.AngleAxis(rotationAngle, rotationAxis) * startRotation;
+        float t = 0f;
+        while (t < rotationTime)
+        {
+            t += Time.deltaTime;
+            transform.RotateAround(targetPosition, rotationAxis, rotationAngle * Time.deltaTime / rotationTime);
+            yield return null;
+        }
+        transform.rotation = endRotation;
+        isRotating = false;
+
+    }
+
 }
    
 
