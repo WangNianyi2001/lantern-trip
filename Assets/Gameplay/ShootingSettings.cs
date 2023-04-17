483442f7dc846a3166abaefeb23dfced00a7db98
Nianyi Wang <Wangnianyi2001@outlook.com>
2023-04-17 16:28:56 +0800

    Fix shooting

--- Assets/Gameplay/ShootingSettings.cs
+++ Assets/Gameplay/ShootingSettings.cs
@@ -4,6 +4,7 @@ using NaughtyAttributes;
 namespace LanternTrip {
 	[CreateAssetMenu(menuName = "LanternTrip/ShootingSettings")]
 	public class ShootingSettings : ScriptableObject {
+		public Vector3 outPosition;
 		[MinMaxSlider(0, 20)] public Vector2 range;
 		[Min(0)] public float maxTime;
 		[Min(0)] public float maxSlope;
