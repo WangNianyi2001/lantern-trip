7eab16db1b4e8216db622b4bec59e2f1cfbecf47
heskey0 <3495759699@qq.com>
2023-06-06 21:27:01 +0800

    face player all the time

--- Assets/Scripts/EnemyAI/Enermy.cs
+++ Assets/Scripts/EnemyAI/Enermy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
 using LanternTrip;
 using UnityEngine;
 
@@ -9,7 +10,23 @@ public class Enermy : MonoBehaviour
     public float Hp = 100f;
 
     public bool Invincible = false;
+
+    public GameObject player;
     
+    private void Update()
+    {
+        LookAtPlayer();
+    }
+
+    public void LookAtPlayer()
+    {
+        if (player == null) return;
+        var dir = (player.transform.position - transform.position);
+        dir.y = 0;
+        dir = dir / Vector3.Magnitude(dir);
+        var angle = Vector3.Dot(transform.right, dir);
+        transform.Rotate(Vector3.up, angle);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
