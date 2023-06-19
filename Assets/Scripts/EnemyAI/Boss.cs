5b12c18990a7b5742f9c9b0602382ec9532fdaef
heskey0 <3495759699@qq.com>
2023-06-19 21:59:53 +0800

    *****

--- Assets/Scripts/EnemyAI/Boss.cs
+++ Assets/Scripts/EnemyAI/Boss.cs
@@ -25,7 +25,12 @@ public class Boss : Enermy
         {
             OnAttack(value);
         });
+        
+        
+        
     }
+
+    
 
     private Tinder.Type GetRandomTinderType()
     {
